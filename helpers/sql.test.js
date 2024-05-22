aba9920686f11ae451faf357e57de4252c36fd35
Anya Aven <leafeon96@gmail.com>
2024-05-22 15:30:49 -0400

    Added valid and 400 test

--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -1,5 +1,31 @@
 import { describe, test, expect } from "vitest";
+import { sqlForPartialUpdate } from "./sql.js";
+import { BadRequestError } from "../expressError.js";
 
 describe("sqlForPartialUpdate", function () {
-  test("")
+  test("returns valid object", function () {
+    const dataToUpdate = { handle: "newName", numEmployees: 10 };
+    const jsToSql = { numEmployees: "num_employees" };
+
+    const result = sqlForPartialUpdate(dataToUpdate, jsToSql);
+
+    expect(result).toEqual(
+      {
+        "setCols": "\"handle\"=$1, \"num_employees\"=$2",
+        "values": ["newName", 10]
+      }
+    )
+  });
+
+  test("throws bad error if dataToUpdate is empty", function () {
+    const dataToUpdate = {};
+    const jsToSql = { numEmployees: "num_employees" };
+
+    let result;
+    try {
+      result = sqlForPartialUpdate(dataToUpdate, jsToSql);
+    } catch (err) {
+      expect(err instanceof BadRequestError).toBeTruthy()
+    }
+  });
 });
