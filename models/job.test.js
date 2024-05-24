11a9eb80e8f3b5efb5da57441f5dbac2589f18df
Jacob <jacobrashid404@gmail.com>
2024-05-24 12:29:45 -0700

    created test file for job model, added create test

---
+++ models/job.test.js
@@ -0,0 +1,57 @@
+import {
+  describe,
+  test,
+  expect,
+  beforeAll,
+  afterAll,
+  beforeEach,
+  afterEach,
+} from "vitest";
+
+import db from "../db.js";
+import { BadRequestError, NotFoundError } from "../expressError.js";
+
+import Job from "./job.js";
+import {
+  commonAfterAll,
+  commonAfterEach,
+  commonBeforeAll,
+  commonBeforeEach,
+} from "./_testCommon.js";
+
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
+
+/************************************** create */
+
+describe("create", function() {
+  const newJob = {
+    title: "new",
+    salary: 100000,
+    equity: 0.25,
+    companyHandle: "c1",
+  };
+  
+  test("works", async function(){
+    let job = await Job.create(newJob);
+    expect(job).toEqual(newJob);
+    
+    const result = await db.query(
+      `SELECT id, title, salary, equity, company_handle
+      FROM jobs
+      WHERE title = 'new'`);
+    expect(result.rows).toEqual([
+      {
+        id: expect.any(Number),
+        title: "new",
+        salary: 100000,
+        equity: "0.25",
+        company_handle: "c1"
+      },
+    ]);
+  });
+});
+
+/************************************** findAll */
