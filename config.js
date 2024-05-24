12dac0d5681854e13239c18e81cec4bb4f7c023c
Anya Aven <leafeon96@gmail.com>
2024-05-24 14:32:10 -0400

    Added ignore for secret

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import "colors";
 
 dotenv.config();
-
+/* v8 ignore next 1 */
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 const PORT = +process.env.PORT || 3001;
 
